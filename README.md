f0283a9f18a0393efd9b57824682554657451288
AtilaDev <atiladevelop@gmail.com>
2019-02-04 11:24:29 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,121 @@
+# expo-calendar-helper
+
+This package will help you to ask permissions to access to calendar and reminders, create new calendar, add events to calendar and delete that one by calendarId.
+
+### Instalation
+
+```
+$ npm install expo-calendar-helper
+or
+$ yarn add expo-calendar-helper
+```
+
+#
+
+### Usage
+
+#### To ask permissions and create a new calendar
+
+```
+import { askPermissionsAsync, newCalendar } from 'expo-calendar-helper';
+
+createCalendar = async () => {
+    const status = await askPermissionsAsync();
+    if (status) {
+      newCalendar('MAZINGER Z','lightgreen', 'mazingerSecureStore');
+    } else {
+      alert(
+        'The app need permissions to access to calendar.'
+      );
+    }
+  };
+```
+
+## askPermissionsAsync():
+
+return granted string if you gave permissions to access calendar, return null if you not.
+
+## newCalendar():
+
+### create a new calendar.
+
+- calendarTitle: Calendar title
+- calendarColor: Calendar color
+- nameCalendarStore: name to save in the mobile using Expo.SecureStore
+  inside "nameCalendarStore" is stored the calendarId, you can get it using
+  Expo.SecureStore.getItemAsync()
+
+#
+
+#### To add a new event
+
+```
+import { addEventsToCalendar } from 'expo-calendar-helper';
+
+addEvent = (nameCalendarStore, contactName, startDate, endDate) => {
+    try {
+      addEventsToCalendar(nameCalendarStore, contactName, startDate, endDate);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+```
+
+## addEventsToCalendar():
+
+### add an event to calendar
+
+this receive four parameters:
+
+- nameCalendarStore: name to load the calendarId from the mobile using
+  Expo.SecureStore saved in the mobile created using newCalendar()
+- eventTitle: event title
+- eventStartDate: event start date
+- eventEndDate: event end date
+
+#
+
+#### To delete a calendar
+
+```
+import { deleteCalendarId } from 'expo-calendar-helper';
+
+deleteCalendarId(nameCalendarStore);
+```
+
+## deleteCalendarId():
+
+### delete a calendar
+
+this receive one parameter:
+
+- nameCalendarStore: name to load the calendarId from the mobile using
+  Expo.SecureStore saved in the mobile created using newCalendar()
+
+#
+
+## Thanks to:
+
+#### Stanisław Chmiela
+
+for helping me create a new calendar on iOS.
+
+#### SleepyFanjo
+
+for sharing how to create an event on iOS.
+
+#### Expo Team
+
+for creating a great framework to work with native react.
+
+#
+
+## All collaborations are welcome!
+
+### If you find this helper useful, will you help me with a star? Thank you!
+
+#
+
+## Licensing
+
+This project is licensed under MIT license.
---
+++ index.js
@@ -0,0 +1,159 @@
+import { Platform } from 'react-native';
+import { Permissions, Calendar, SecureStore, Localization } from 'expo';
+
+_askForCalendarPermissions = async () => {
+  const response = await Permissions.askAsync(Permissions.CALENDAR);
+  return response.status === 'granted';
+};
+
+_askForReminderPermissions = async () => {
+  if (Platform.OS === 'android') {
+    return true;
+  }
+
+  const response = await Permissions.askAsync(Permissions.REMINDERS);
+  return response.status === 'granted';
+};
+
+_findCalendars = async () => {
+  const calendarGranted = await this._askForCalendarPermissions();
+  const reminderGranted = await this._askForReminderPermissions();
+  let calendars = [];
+
+  if (calendarGranted && reminderGranted) {
+    calendars = await Calendar.getCalendarsAsync();
+  }
+
+  return calendars;
+};
+
+_createNewCalendar = async (calendars, calendarTitle, calendarColor) => {
+  const newCalendar = {
+    title: calendarTitle,
+    entityType: Calendar.EntityTypes.EVENT,
+    color: calendarColor,
+    sourceId:
+      Platform.OS === 'ios'
+        ? calendars.find(cal => cal.source && cal.source.name === 'iCloud')
+            .source.id
+        : undefined,
+    source:
+      Platform.OS === 'android'
+        ? {
+            name: calendars.find(
+              cal => cal.accessLevel === Calendar.CalendarAccessLevel.OWNER
+            ).source.name,
+            isLocalAccount: true
+          }
+        : undefined,
+    name: calendarTitle,
+    accessLevel: Calendar.CalendarAccessLevel.OWNER,
+    ownerAccount:
+      Platform.OS === 'android'
+        ? calendars.find(
+            cal => cal.accessLevel == Calendar.CalendarAccessLevel.OWNER
+          ).ownerAccount
+        : undefined
+  };
+
+  let calendarId = null;
+
+  try {
+    calendarId = await Calendar.createCalendarAsync(newCalendar);
+  } catch (e) {
+    // console.log(e.message);
+  }
+
+  return calendarId;
+};
+
+// --------------------------------------------------------------------------
+// askPermissionsAsync(): ask for permissions to access to calendar
+// --------------------------------------------------------------------------
+// return granted string if you gave permissions to access calendar,
+// return null if you not
+// --------------------------------------------------------------------------
+export const askPermissionsAsync = async () => {
+  const calendarGranted = await this._askForCalendarPermissions();
+  const reminderGranted = await this._askForReminderPermissions();
+
+  if (calendarGranted && reminderGranted) {
+    return 'granted';
+  } else {
+    return null;
+  }
+};
+
+// --------------------------------------------------------------------------
+// newCalendar(): create a new Calendar
+// --------------------------------------------------------------------------
+// this receive three parameters:
+//
+// * calendarTitle: Calendar title
+// * calendarColor: Calendar color
+// * nameCalendarStore: name to save in the mobile using Expo.SecureStore
+//
+// inside <nameCalendarStore> is stored the calendarId, you can get it using
+// Expo.SecureStore.getItemAsync()
+// --------------------------------------------------------------------------
+export const newCalendar = async (
+  calendarTitle,
+  calendarColor,
+  nameCalendarStore
+) => {
+  let calendars = await this._findCalendars();
+  const calendarId = await this._createNewCalendar(
+    calendars,
+    calendarTitle,
+    calendarColor
+  );
+  await SecureStore.setItemAsync(nameCalendarStore, calendarId);
+};
+
+// --------------------------------------------------------------------------
+// deleteCalendarId(): delete a calendar
+// --------------------------------------------------------------------------
+// this receive one parameter:
+//
+// * nameCalendarStore: name to load the calendarId from the mobile using
+//   Expo.SecureStore saved in the mobile created using newCalendar()
+// --------------------------------------------------------------------------
+export const deleteCalendarId = async nameCalendarStore => {
+  const calendarId = await SecureStore.getItemAsync(nameCalendarStore);
+  if (calendarId) {
+    await Calendar.deleteCalendarAsync(calendarId);
+    await SecureStore.deleteItemAsync(nameCalendarStore);
+  }
+};
+
+// --------------------------------------------------------------------------
+// addEventsToCalendar(): add an event to calendar
+// --------------------------------------------------------------------------
+// this receive four parameters:
+//
+// * nameCalendarStore: name to load the calendarId from the mobile using
+//   Expo.SecureStore saved in the mobile created using newCalendar()
+// * eventTitle: event title
+// * eventStartDate: event start date
+// * eventEndDate: event end date
+// --------------------------------------------------------------------------
+export const addEventsToCalendar = async (
+  nameCalendarStore,
+  eventTitle,
+  eventStartDate,
+  eventEndDate
+) => {
+  const calendarId = await SecureStore.getItemAsync(nameCalendarStore);
+  const event = {
+    title: eventTitle,
+    startDate: eventStartDate,
+    endDate: eventEndDate,
+    timeZone: Localization.timezone
+  };
+
+  try {
+    await Calendar.createEventAsync(calendarId, event);
+  } catch (e) {
+    // console.log(e);
+  }
+};
